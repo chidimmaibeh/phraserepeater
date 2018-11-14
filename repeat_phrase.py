5d0c075de9f5dce06333bf84488a468ee1c9ae8d
chidimma ibeh <chidimmai@gmail.com>
2018-11-14 02:59:56 -0600

    Phrase Repeater

---
+++ repeat_phrase.py
@@ -0,0 +1,15 @@
+# this program will repeat a phrase 100 times.
+#the program will start at index 1 and stop at 100.
+
+def repeat_phrase():
+ 
+     for index in range(1, 101):
+        print(index, "I will never spam my friends again.")
+    
+
+def main():
+    
+    repeat_phrase()
+
+
+main()
